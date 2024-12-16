b61eadfb06ec18b5e5647180405e9dc27b00d571
DaHub49 <dyls.martin.49@gmail.com>
2024-12-16 08:55:50 +0200

    16/12/24 @08:55: Video 109, Day 7:
    Moved ShoppingList Data Class to a separate file.
    
    Moved the Column from MainActivity.kt to ShoppingList.kt and put it inside the newly created @Composable ShoppingListApp().

--- app/src/main/java/za/co/howtogeek/myshoppinglistapp/MainActivity.kt
+++ app/src/main/java/za/co/howtogeek/myshoppinglistapp/MainActivity.kt
@@ -4,23 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import za.co.howtogeek.myshoppinglistapp.ui.theme.MyShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,36 +20,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
-                    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center
-                        ) {
-                        Button(
-                            onClick = {},
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
-                        ) {
-                            Text("Add Item")
-                        }
-                        LazyColumn(
-                            modifier = Modifier.fillMaxSize().padding(16.dp)
-                        ) {
-                            items(sItems){
-
-                            }
-                        }
-
-                    }
+                    ShoppingListApp()
                 }
             }
         }
     }
 }
-
-data class ShoppingItem(val id: Int,
-                        var name: String,
-                        var quantity: Int,
-                        var isEditing: Boolean = false
-)
 
