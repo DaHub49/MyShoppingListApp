44eeba47c70bd3ef3f5f486022b2b533ad59f677
DaHub49 <dyls.martin.49@gmail.com>
2024-12-18 07:00:21 +0200

    18/12/24 @06:57: [experimenting with what I've learnt in] [Video 113, Day 7]:
    -> Successfully experimented with different modifier elements and also changed the way the ShoppingListItem is displayed.

--- app/src/main/java/za/co/howtogeek/myshoppinglistapp/ShoppingList.kt
+++ app/src/main/java/za/co/howtogeek/myshoppinglistapp/ShoppingList.kt
@@ -1,16 +1,14 @@
 package za.co.howtogeek.myshoppinglistapp
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
@@ -22,7 +20,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(val id: Int,
@@ -40,8 +37,14 @@ fun ShoppingListApp() {
 
 
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding(),
+            //.padding(16.dp),
+            //.background(Color.White),
+                // ... other modifiers,
+        verticalArrangement = Arrangement.Center,
+
     ) {
         Button(
             onClick = {showDialog = true},
@@ -63,7 +66,9 @@ fun ShoppingListApp() {
         AlertDialog(
             onDismissRequest = { showDialog = false},
             confirmButton = {
-                Row(modifier = Modifier.fillMaxWidth().padding(8.dp),
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(8.dp),
                     horizontalArrangement = Arrangement.SpaceBetween) {
                     Button(onClick = {
                         if (itemName.isNotBlank()){
@@ -116,12 +121,13 @@ fun ShoppingListItem(
     onDeleteClick: () -> Unit,
 ){
     Row(
-        modifier = Modifier.padding(8.dp).fillMaxWidth().border(
+        modifier = Modifier.padding(8.dp)/*.fillMaxWidth().border(
             border = BorderStroke(2.dp, Color (0XFF018786)),
             shape = RoundedCornerShape(20)
         )
+        */
     ) {
-        Text(text = item.name,
+        Text(text = "${item.name} -> ",
             modifier = Modifier.padding(8.dp)
         )
         Text(
