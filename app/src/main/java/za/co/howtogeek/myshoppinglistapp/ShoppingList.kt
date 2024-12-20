42871dad5626a039a1b1aa78f15182d40b03c395
DaHub49 <dyls.martin.49@gmail.com>
2024-12-20 06:52:45 +0200

    20/12/24 @06:09: [:] [end of Video 120, Day 7]:
    -> Implemented the .find{} function in sItems.
    -> setup the onEditClick and onDeleteClick functions.
    -> finished app according to tut.

--- app/src/main/java/za/co/howtogeek/myshoppinglistapp/ShoppingList.kt
+++ app/src/main/java/za/co/howtogeek/myshoppinglistapp/ShoppingList.kt
@@ -19,7 +19,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
